19d614461480ee543d719cbcc42e0ee8f164f511
Alexey Horoshavin <Akela.Klimov@yandex.ru>
2022-11-17 22:06:23 +0700

    Update project

--- test-methods-project/tm-e2e-tests/src/test/java/org/nsu/fit/services/rest/data/CustomerPojo.java
+++ test-methods-project/tm-e2e-tests/src/test/java/org/nsu/fit/services/rest/data/CustomerPojo.java
@@ -1,11 +1,13 @@
 package org.nsu.fit.services.rest.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CustomerPojo extends ContactPojo{
     @JsonProperty("id")
     public UUID id;
